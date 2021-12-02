4a80d2e2298e82ab10ca94b8a84df913d2696156
Johnson <weihsun.chen@nogle.com>
2021-12-02 12:24:58 +0800

    fix document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,26 +1,33 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from 'next/document';
 
 class MyDocument extends Document {
-  static async getInitialProps(ctx) {
+  static async getInitialProps(ctx: DocumentContext) {
     const initialProps = await Document.getInitialProps(ctx);
     return { ...initialProps };
   }
-
-  return() {
-    <Html>
-      <Head>
-        <link
-          rel="icon"
-          type="image/png"
-          href="/imgs/favicon.png"
-          sizes="32*32"
-        />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>;
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            rel="icon"
+            type="image/png"
+            href="/imgs/favicon.png"
+            sizes="32*32"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
   }
 }
 
