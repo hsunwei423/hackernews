31786a56c4f0ab547779cf55d21496612dc2871c
Johnson <weihsun.chen@nogle.com>
2021-12-02 10:57:17 +0800

    update eslint and prettier

--- .eslintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  parser: "@typescript-eslint/parser",
-  plugins: [
-    "@typescript-eslint",
-    "eslint-plugin-react"
-  ],
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"]
-}
