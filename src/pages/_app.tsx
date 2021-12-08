7c2689f1a3148025df5618b547cdc8fa182fb011
weihsun <johnson823423@gmail.com>
2021-12-08 23:42:02 +0800

    use normalize css and modify scroll bar style

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import { AppProps } from 'next/app';
-import 'styles/global-styles.css';
+import 'styles/global.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   // common css
