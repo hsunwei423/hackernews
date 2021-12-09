0cb1df07dbb858c48bdd783a10fb210d6bc46cf1
weihsun <johnson823423@gmail.com>
2021-12-09 23:30:09 +0800

    Deploy Next.js to gh-pages

---
+++ out/_next/static/development/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/best":["static\u002Fchunks\u002Fpages\u002Fbest.js"],"/top":["static\u002Fchunks\u002Fpages\u002Ftop.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fbest","\u002Ftop"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
