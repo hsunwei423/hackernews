53c926b6a5e2de0fdf6c8999efcddef953a98336
Johnson <weihsun.chen@nogle.com>
2021-12-15 10:59:46 +0800

    handle close menu flow

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,15 +1,31 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import { useAppDispatch } from 'hooks/reduxHook';
 
 import Header from 'components/Header';
 import Drawer from 'components/Drawer';
 
 import { ROUTE_LIST } from 'consts';
+import { closeDrawer } from 'reducers/drawer';
 
 import style from './layout.module.scss';
+import { useEffect } from 'react';
 
 export function Layout({ children }) {
   const router = useRouter();
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    const handleRouteStart = () => {
+      dispatch(closeDrawer());
+    }
+
+    router.events.on('routeChangeStart', handleRouteStart);
+
+    return () => {
+      router.events.off('routeChangeStart', handleRouteStart); 
+    }
+  }, [router]);
 
   const renderRouteList = () => {
     const currentPath = router.pathname;
