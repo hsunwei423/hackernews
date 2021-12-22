01e7ec3262630768d266d972a0a185ec24e394d6
Johnson <weihsun.chen@nogle.com>
2021-12-22 17:13:30 +0800

    handle load more comment flow to improve performance

--- src/components/Comment/index.tsx
+++ src/components/Comment/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import useSWR from 'swr';
 import apiInstance from 'api';
 
@@ -12,41 +12,67 @@ type CommentProp = {
   idList: string[]
 }
 
-const Comment: FC<CommentProp> = ({ idList }) => {
-  const renderComment = (id: string) => {
-    const { data, error } = useSWR(
-      `/item/${id}.json`,
-      fetcher
-    );
+type SingleProp = {
+  id: string
+}
 
-    if (error) {
-      return 'something went wrong';
-    }
+const LIMIT_SIZE = 2;
 
-    return (
-      <div className={style.container}>
-        <Avatar author={data?.by} />
-        <div className={style.content}>
-          <span className={style.name}>{data?.by}</span>
-          <div className={style.htmlContent} dangerouslySetInnerHTML={{__html: data?.text}} />
-          <div className={`${style.name} ${style.reply}`}>reply to</div>
+const SingleComment: FC<SingleProp> = ({ id }) => {
+  const { data, error } = useSWR(
+    `/item/${id}.json`,
+    fetcher
+  );
 
-          {/** children comment */}
-          {
-            data?.kids && <Comment idList={data?.kids} />
-          }
-        </div>
+  if (error) {
+    return <div>something went wrong</div>;
+  }
+
+  return (
+    <div className={style.container} key={id}>
+      <Avatar author={data?.by} />
+      <div className={style.content}>
+        <span className={style.name}>{data?.by}</span>
+        <div className={style.htmlContent} dangerouslySetInnerHTML={{__html: data?.text}} />
+        <div className={`${style.name} ${style.reply}`}>reply to</div>
+
+        {/** children comment */}
+        {
+          data?.kids && <Comment idList={data?.kids} />
+        }
       </div>
-    )
+    </div>
+  )
+}
+
+const Comment: FC<CommentProp> = ({ idList }) => {
+  const [isExpand, setExpand] = useState<boolean>(false);
+
+  const handleExpand = () => {
+    setExpand(true);
   };
+
+  const renderList = () => {
+    const len = idList.length;
+    if (len > LIMIT_SIZE && !isExpand) {
+      return (
+        <>
+          {
+            idList
+              .slice(0, LIMIT_SIZE)
+              .map(id => <SingleComment key={id} id={id} />)
+          }
+          <div className={style['load-more']} onClick={handleExpand}>load more</div>
+        </>
+      )
+    } else {
+      return idList.map(id => <SingleComment key={id} id={id} />);
+    }
+  }
 
   return (
     <div className={style.wrapper}>
-      {
-        idList.map(id => {
-          return renderComment(id);
-        })
-      }
+      { renderList() }
     </div>
   )
 }
